275ff3e421d2fc96895cb1967bade28201dfff43
Nikita <nikita.dronoff@gmail.com>
2023-04-15 15:27:41 +0600

    - добавил миграцию

---
+++ MobileStore/Migrations/20230414184325_added new field in Brand model.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MobileStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class addednewfieldinBrandmodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateEstablishment",
+                table: "Brands",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Brands",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateEstablishment",
+                table: "Brands");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Brands");
+        }
+    }
+}
